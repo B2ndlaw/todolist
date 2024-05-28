6ec45235d599d9a06347ef7e3f099de58f9e1f74
Aleksey <115175632+B2ndlaw@users.noreply.github.com>
2024-05-28 11:11:30 +0300

    test

---
+++ src/01.test.ts
@@ -0,0 +1,26 @@
+import { mult, sum } from "./01";
+
+test("sum should be correct", () => {
+ //data
+  const a = 1;
+  const b = 2;
+  const c = 3;
+
+  //action
+  const result1 = sum(a, b)
+  const result2 = sum(a, c)
+//expect result
+  expect(result1).toBe(3)
+  expect(result2).toBe(4)
+})
+
+test("mult should be correct", () => {
+  const a = 1;
+  const b = 2;
+  const c = 3;
+  const result1 = mult(a, b)
+  const result2 = mult(a, c)
+
+  expect(result1).toBe(2)
+  expect(result2).toBe(3)
+})
---
+++ src/01.ts
@@ -0,0 +1,13 @@
+export function sum(a: number, b: number) {
+  return a + b;
+}
+
+export function mult(a: number, b: number) {
+    return a * b;
+  }
+
+const sentence = "Hello my friend";
+
+function splitIntoWords() {
+  return ["hello", "my", "friend"];
+}
